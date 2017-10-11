07eea608382837e62e3dd53abd72481f216fb2a4
Yuan-Yi Chang <changyy.csie@gmail.com>
2017-10-11 15:28:31 +0000

    add simple test

--- setup.py
+++ setup.py
@@ -79,10 +79,11 @@ setup(
     # dependencies). You can install these using the following syntax,
     # for example:
     # $ pip install -e .[dev,test]
-    #extras_require={
+    extras_require={
     #    'dev': ['check-manifest'],
     #    'test': ['coverage'],
-    #},
+        'test': ['pandas','numpy','sklearn'],
+    },
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
--- tests/test_simple.py
+++ tests/test_simple.py
@@ -2,6 +2,52 @@
 # testing in general, but rather to support the `find_packages` example in
 # setup.py that excludes installing the "tests" package
 
-
 def test_success():
-    assert True
+	assert True
+
+def test_feature_engineering():
+	import pandas as pd
+	# https://archive.ics.uci.edu/ml/datasets/Iris
+	dataset = pd.read_csv(
+		'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', 
+		# https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.names
+		names = [ 'sepal length in cm', 'sepal width in cm', 'petal length in cm', 'petal width in cm', 'class']
+	)
+	print()
+	print("dataset:", dataset.shape)
+	assert dataset.shape[0] > 100
+	print("columns:\n", dataset.columns)
+
+	from org.changyy.ml.helper import data_numeric_handler_process
+
+	print("raw:\n\n", dataset.head(3))
+	_, handled_dataset = data_numeric_handler_process(dataset.copy(), skip_columns=['class'])
+	print("Encoding:\n\n", handled_dataset.head(3))
+
+	assert set(handled_dataset['class'].unique()) == set(dataset['class'].unique())
+	#print(handled_dataset['class'].unique())
+	for column in handled_dataset.columns:
+		assert len(handled_dataset[column].unique()) == len(dataset[column].unique())
+		if column != 'class':
+			assert set(handled_dataset[column].unique()) != set(dataset[column].unique())
+
+	y = handled_dataset['class']
+	train = handled_dataset.drop(['class'], axis=1) 
+	from sklearn.model_selection import train_test_split
+	X_train, X_test, y_train, y_test = train_test_split(train, y, random_state=0)
+
+	from sklearn.multiclass import OneVsOneClassifier
+	from sklearn.svm import LinearSVC
+	classifier = OneVsOneClassifier(LinearSVC(random_state = 0))
+
+	classifier.fit(X_train, y_train)
+	expected = y_test
+	predicted = classifier.predict(X_test)
+
+	from sklearn import metrics
+	print("Classification report for classifier %s:\n%s\n" % (classifier, metrics.classification_report(expected, predicted)))
+
+	score = classifier.score(X_test, y_test)
+	print("Score:", score)
+	assert round(score, 10) == round(0.84210526315, 10)
+
